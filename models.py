eb44f149984de937ffea998a73c461cdddf2e294
zhuochen <zhuochen777@gmail.com>
2023-01-18 20:16:00 -0800

    modify database path

--- models.py
+++ models.py
@@ -9,6 +9,8 @@ import json
 # database_path = 'postgresql://{}@{}/{}'.format(DB_USER, DB_HOST, DB_NAME)
 
 database_path = os.environ['DATABASE_URL']
+if database_path.startswith("postgres://"):
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
 
 db = SQLAlchemy()
 
